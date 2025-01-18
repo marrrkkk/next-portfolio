ee9076b0202751965ae93ee277159ed3f1014232
marrrkkk <definitelynotmark13@gmail.com>
2025-01-18 16:28:06 +0800

    feat: add email link to Hire Me button in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,21 @@
+import Link from "next/link";
 import { RainbowButton } from "./ui/rainbow-button";
 
 const Footer = () => {
-  return(
+  return (
     <footer className="w-full">
       <div className="my-40 text-center">
         <p className="text-zinc-500">Need a Developer?</p>
         <h1 className="font-bold text-4xl mb-5">Let&apos;s Work Together</h1>
-        <RainbowButton>Hire Me</RainbowButton>
+        <RainbowButton>
+          <Link href="mailto:definitelynotmark13@gmail.com">Hire Me</Link>
+        </RainbowButton>
       </div>
       <div className="ml-10 mb-5 text-zinc-500 text-sm">
         <p>© 2024 Mark Louie Alvarez</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
