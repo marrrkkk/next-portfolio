7a1410696df44345a181e62222cfa194e0276105
marrrkkk <definitelynotmark13@gmail.com>
2026-06-06 20:01:58 +0800

    refactor: restructure main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-import Skills from "@/components/Tech";
 import Nav from "@/components/Nav";
-import Projects from "@/components/Projects";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
+import dynamic from "next/dynamic";
+
+const Skills = dynamic(() => import("@/components/Tech"), {
+  loading: () => <div className="mt-40 xl:w-[45%] md:w-[70%] w-[80%] h-64" />,
+});
+const Experience = dynamic(() => import("@/components/Experience"), {
+  loading: () => <div className="mt-20 xl:w-[45%] md:w-[70%] w-[80%] h-64" />,
+});
+const Projects = dynamic(() => import("@/components/Projects"), {
+  loading: () => <div className="mt-20 xl:w-[45%] md:w-[70%] w-[80%] h-64" />,
+});
+const Footer = dynamic(() => import("@/components/Footer"));
 
 const Home = () => {
   return (
